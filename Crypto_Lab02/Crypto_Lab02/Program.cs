e7c2bb01391d18d976d7588c7c2fad34523e1fc3
Maxim <maxim.tickhonovich@gmail.com>
2019-03-05 21:56:01 +0300

    Entropy

--- Crypto_Lab02/Crypto_Lab02/Program.cs
+++ Crypto_Lab02/Crypto_Lab02/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 
 namespace Crypto_Lab02
@@ -13,20 +10,66 @@ namespace Crypto_Lab02
     {
         static void Main(string[] args)
         {
+            //task 1
             Shannon shannon = new Shannon();
-            string path = @"D:\study3course\6thsem\Cryptography\Labs\test.txt";
-            string s;
+            string pathBytes = "bytes.txt";
+            string path = "rfc2616.txt";
+
+            string rfc2616;
             using (StreamReader sr = new StreamReader(path, Encoding.Default))
-            {
-                s = sr.ReadToEnd();
-                Console.WriteLine(s);
-            }
-            string pattern = @"[A-Za-z\s+\W]";
+            { rfc2616 = sr.ReadToEnd(); }
+            string patternRU = @"[A-Za-z0-9\s+\W_]";
+            string patternEN = @"[А-Яа-я0-9\s+\W_]";
+            string patternBIN = @"\s+";
             string target = "";
-            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
-            string result = regex.Replace(s, target);
-            Console.WriteLine($"Энтропия по Шеннону фразы = " + shannon.ShannonEntropy(result.ToLower()));
-            Console.WriteLine(result.ToLower());
+
+            Regex regexRU = new Regex(patternRU, RegexOptions.IgnoreCase);
+            Regex regexEN = new Regex(patternEN);
+            Regex regexBIN = new Regex(patternBIN);
+
+            string resultRU = regexRU.Replace(rfc2616, target);
+            string resultEN = regexEN.Replace(rfc2616, target);
+            string resultBIN = regexBIN.Replace(rfc2616, target);
+
+            //Console.WriteLine("RUS Энтропия по Шеннону фразы = " + shannon.ShannonEntropy(resultRU.ToLower()));
+            Console.WriteLine("ENG Энтропия по Шеннону фразы = " + shannon.ShannonEntropy(resultEN.ToLower()));
+
+            //task 2
+            StringBuilder builder = new StringBuilder();
+            foreach(char a in resultBIN)            
+                builder.Append(Convert.ToString(a, 2));
+
+            using (StreamWriter sw = new StreamWriter(pathBytes, false, Encoding.Default))
+            { sw.Write(builder); }
+            Console.WriteLine("Binary Энторопия по Шенону = " + shannon.ShannonEntropy(builder.ToString()));
+
+            //Console.WriteLine(rfc2616);
+            //Console.WriteLine(builder.ToString());
+            //Console.WriteLine(resultBIN.ToString());
+
+            //task 3
+            String myName = "Tikhonovich Maxim Alexandrovich";
+            string patternName = @"\s+";
+            Regex regexName = new Regex(patternName);
+            string resulName = regexName.Replace(myName, target);
+            double shann = shannon.ShannonEntropy(resultEN.ToLower());
+            Console.WriteLine($"Количество информации в ФИО {shannon.AmountOfInformation(resulName, shann)}");
+            Console.WriteLine(resulName);
+
+            byte[] bytes = Encoding.ASCII.GetBytes(resulName);
+            String ASCII = "";
+            foreach (var b in bytes)
+                ASCII += b;
+
+            //хз каку энтропию для ASCII брать
+            Console.WriteLine("ASCII: Кол-во инф-ции в ФИО " + shannon.AmountOfInformation(ASCII, shann));
+
+            //task4
+            Console.WriteLine("С условной вероятностью ошибки 0,1 " + shannon.AmountOfInformationWithMistake(resulName, 0.9));
+            Console.WriteLine("С условной вероятностью ошибки 0,5 " + shannon.AmountOfInformationWithMistake(resulName, 0.5));
+            Console.WriteLine("С условной вероятностью ошибки 1 " + shannon.AmountOfInformationWithMistake(resulName, 1));
+
+            Console.ReadLine();
         }
     }
 }
--- Crypto_Lab02/Crypto_Lab02/Shannon.cs
+++ Crypto_Lab02/Crypto_Lab02/Shannon.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Crypto_Lab02
 {
     class Shannon
     {
-        public  double ShannonEntropy(string s)
+        public double ShannonEntropy(string s)
         {
             var map = new Dictionary<char, int>();
             foreach (char c in s)
@@ -21,11 +21,34 @@ namespace Crypto_Lab02
 
             double result = 0.0;
             int len = s.Length;
+
+            var orderkey = from i in map orderby i.Key select i;
+            foreach (var item in orderkey)
+            {
+                using (StreamWriter sw = new StreamWriter("info.log", true, Encoding.Default))
+                { sw.Write($"{DateTime.Now}      Количество символов {item.Key}  = {item.Value} \n"); }
+            }
+
             foreach (var item in map)
             {
                 var frequency = (double)item.Value / len;
-                result -= frequency * (Math.Log(frequency) / Math.Log(2));
-            }
+                // result -= frequency * (Math.Log(frequency) / Math.Log(2));
+                result -= frequency * Math.Log(frequency, 2);
+            }                
+            return result;
+        }
+
+        public double AmountOfInformation(string message, double shannonEntropy)
+        {
+            double result = 0.00;
+            result = message.Length * shannonEntropy;
+            return result;
+        }
+
+        public double AmountOfInformationWithMistake(string message, double q)
+        {
+            double result = 0.00;
+            result = 1 - (-(1 - q) * Math.Log((1 - q), 2) - q * Math.Log(q, 2)) * message.Length;
             return result;
         }
     }
