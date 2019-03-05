c0cc9e6cee3e3b63bb1ace0ed18ee646dd52da1d
Maxim <maxim.tickhonovich@gmail.com>
2019-03-05 09:52:33 +0300

    ShannonEntropy

---
+++ Crypto_Lab02/Crypto_Lab02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace Crypto_Lab02
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shannon shannon = new Shannon();
+            string path = @"D:\study3course\6thsem\Cryptography\Labs\test.txt";
+            string s;
+            using (StreamReader sr = new StreamReader(path, Encoding.Default))
+            {
+                s = sr.ReadToEnd();
+                Console.WriteLine(s);
+            }
+            string pattern = @"[A-Za-z\s+\W]";
+            string target = "";
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+            string result = regex.Replace(s, target);
+            Console.WriteLine($"Энтропия по Шеннону фразы = " + shannon.ShannonEntropy(result.ToLower()));
+            Console.WriteLine(result.ToLower());
+        }
+    }
+}
---
+++ Crypto_Lab02/Crypto_Lab02/Shannon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto_Lab02
+{
+    class Shannon
+    {
+        public  double ShannonEntropy(string s)
+        {
+            var map = new Dictionary<char, int>();
+            foreach (char c in s)
+            {
+                if (!map.ContainsKey(c))
+                    map.Add(c, 1);
+                else
+                    map[c] += 1;
+            }
+
+            double result = 0.0;
+            int len = s.Length;
+            foreach (var item in map)
+            {
+                var frequency = (double)item.Value / len;
+                result -= frequency * (Math.Log(frequency) / Math.Log(2));
+            }
+            return result;
+        }
+    }
+}
