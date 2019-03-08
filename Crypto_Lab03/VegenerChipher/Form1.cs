21c973c1b92c307c09bbe780feb6c27bbca56a44
Maxim <maxim.tickhonovich@gmail.com>
2019-03-08 15:13:57 +0300

    Vegener

---
+++ Crypto_Lab03/VegenerChipher/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VegenerChipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Vegener vegener = new Vegener();
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            if (radioButtonGamma.Checked)
+            {
+                string s;
+
+                StreamReader sr = new StreamReader("in.txt", Encoding.GetEncoding(1251));
+                StreamWriter sw = new StreamWriter("out.txt", true, Encoding.GetEncoding(1251));
+
+                while (!sr.EndOfStream)
+                {
+                    s = sr.ReadLine();
+                    sw.WriteLine(vegener.Encode(s, vegener.Generate_Pseudorandom_KeyWord(s.Length, 100)));
+                }
+
+                sr.Close();
+                sw.Close();
+            }
+            else
+            {
+                if (textBoxKeyWord.Text.Length > 0)
+                {
+                    string s;
+
+                    StreamReader sr = new StreamReader("in.txt", Encoding.GetEncoding(1251));
+                    StreamWriter sw = new StreamWriter("out.txt", true, Encoding.GetEncoding(1251));
+
+                    while (!sr.EndOfStream)
+                    {
+                        s = sr.ReadLine();
+                        sw.WriteLine(vegener.Encode(s, textBoxKeyWord.Text));
+                    }
+
+                    sr.Close();
+                    sw.Close();
+                    MessageBox.Show("Сообщение зашифровано");
+                }
+                else
+                    MessageBox.Show("Введите ключевое слово!");
+            }
+        }
+
+        private void buttonDecipher_Click(object sender, EventArgs e)
+        {
+            if (radioButtonGamma.Checked)
+            {
+                string s;
+
+                StreamReader sr = new StreamReader("out.txt", Encoding.GetEncoding(1251));
+                StreamWriter sw = new StreamWriter("decode.txt", true, Encoding.GetEncoding(1251));
+
+                while (!sr.EndOfStream)
+                {
+                    s = sr.ReadLine();
+                    sw.WriteLine(vegener.Decode(s, vegener.Generate_Pseudorandom_KeyWord(s.Length, 100)));
+                }
+
+                sr.Close();
+                sw.Close();
+            }
+            else
+            {
+                if (textBoxKeyWord.Text.Length > 0)
+                {
+                    string s;
+
+                    StreamReader sr = new StreamReader("out.txt", Encoding.GetEncoding(1251));
+                    StreamWriter sw = new StreamWriter("decode.txt", true, Encoding.GetEncoding(1251));
+
+                    while (!sr.EndOfStream)
+                    {
+                        s = sr.ReadLine();
+                        sw.WriteLine(vegener.Decode(s, textBoxKeyWord.Text));
+                    }
+
+                    sr.Close();
+                    sw.Close();
+                    MessageBox.Show("Сообщение расшифровано");
+                }
+                else
+                    MessageBox.Show("Введите ключевое слово!");
+            }
+        }
+    }
+}
---
+++ Crypto_Lab03/VegenerChipher/Vegener/Vegener.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace VegenerChipher
+{
+    class Vegener
+    {
+        static char[] characters = new char[] { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+                                                'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                                'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
+                                                '8', '9', '0' };
+        int N = characters.Length;
+
+        public string Encode(string input, string keyword)
+        {
+            input = input.ToUpper();
+            keyword = keyword.ToUpper();
+
+            string result = "";
+
+            int keyword_index = 0;
+
+            foreach (char symbol in input)
+            {
+                int c = (Array.IndexOf(characters, symbol) +
+                    Array.IndexOf(characters, keyword[keyword_index])) % N;
+
+                result += characters[c];
+
+                keyword_index++;
+
+                if ((keyword_index + 1) == keyword.Length)
+                    keyword_index = 0;
+            }
+
+            return result;
+        }
+
+
+        public string Decode(string input, string keyword)
+        {
+            input = input.ToUpper();
+            keyword = keyword.ToUpper();
+
+            string result = "";
+
+            int keyword_index = 0;
+
+            foreach (char symbol in input)
+            {
+                int p = (Array.IndexOf(characters, symbol) + N -
+                    Array.IndexOf(characters, keyword[keyword_index])) % N;
+
+                result += characters[p];
+
+                keyword_index++;
+
+                if ((keyword_index + 1) == keyword.Length)
+                    keyword_index = 0;
+            }
+
+            return result;
+        }
+
+
+        public string Generate_Pseudorandom_KeyWord(int length, int startSeed)
+        {
+            Random rand = new Random(startSeed);
+
+            string result = "";
+
+            for (int i = 0; i < length; i++)
+                result += characters[rand.Next(0, characters.Length)];
+
+            return result;
+        }
+    }
+}
