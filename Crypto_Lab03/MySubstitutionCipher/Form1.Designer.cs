173e1c4b1c8c2f1c5b2cdeadc2ca43aa5bbf9508
Maxim <maxim.tickhonovich@gmail.com>
2019-03-08 22:01:15 +0300

    simple cipher

---
+++ Crypto_Lab03/MySubstitutionCipher/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MySubstitutionCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 374);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(239, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(222, 374);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(467, 12);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(222, 374);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(379, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(181, 46);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Шифр простой замены";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Crypto_Lab03/MySubstitutionCipher/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MySubstitutionCipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = File.ReadAllText("text.txt", Encoding.GetEncoding(1251));
+        }
+ 
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] alf = File.ReadAllLines("alf.txt", Encoding.GetEncoding(1251));
+                string[] key = File.ReadAllLines("key.txt", Encoding.GetEncoding(1251));
+                string text = File.ReadAllText("text.txt", Encoding.GetEncoding(1251));
+                var substitution = new Dictionary<string, string>();
+
+                for (int i = 0; i < alf.Length; i++)                
+                    substitution[alf[i]] = key[i];
+                
+                StringBuilder ans = new StringBuilder();
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (substitution.ContainsKey(text[i].ToString()))                    
+                        ans.Append(substitution[text[i].ToString()]);                    
+                    else
+                        ans.Append(text[i]);
+                    textBox2.Text = ans.ToString();
+                }
+                File.WriteAllText("code.txt", ans.ToString(), Encoding.GetEncoding(1251));
+            }
+            catch { }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] alf = File.ReadAllLines("alf.txt", Encoding.GetEncoding(1251));
+            string[] key = File.ReadAllLines("key.txt", Encoding.GetEncoding(1251));
+            string code = File.ReadAllText("code.txt", Encoding.GetEncoding(1251));
+            var substitution = new Dictionary<string, string>();
+
+            for (int i = 0; i < alf.Length; i++)
+                substitution[key[i]] = alf[i];
+
+            StringBuilder ans = new StringBuilder();
+            for (int i = 0; i < code.Length; i++)
+            {
+                if (substitution.ContainsKey(code[i].ToString()))
+                    ans.Append(substitution[code[i].ToString()]);
+                else
+                    ans.Append(code[i]);
+                textBox3.Text = ans.ToString();
+            }
+            File.WriteAllText("decode.txt", ans.ToString(), Encoding.GetEncoding(1251));
+
+        }
+    }
+}
