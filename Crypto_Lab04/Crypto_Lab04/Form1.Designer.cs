989f3dacebaf845b16130ff9b0ce8786e219bb31
Maxim <maxim.tickhonovich@gmail.com>
2019-03-20 00:42:14 +0300

    Lab_04

---
+++ Crypto_Lab04/Crypto_Lab04/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Crypto_Lab04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 83);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(463, 317);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Исходный текст";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(451, 283);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 455);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Шифр перестановки";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Crypto_Lab04/Crypto_Lab04/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Crypto_Lab04
+{
+    public partial class Form1 : Form
+    {
+        int state;
+        int[] key;
+
+        public Form1()
+        {
+            InitializeComponent();
+            state = 1;
+            int[] k = { 2, 5, 4, 3, 1 };
+            key = k;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        int[] getunkey(int[] array)
+        {
+            int[] res = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                res[array[i] - 1] = i + 1;
+            }
+            return res;
+        }
+        string crypt(string msg, bool flag)
+        {
+            string tmp1;
+            string tmp2;
+            int[] k;
+
+            if (flag)
+            {
+                k = key;
+            }
+            else
+            {
+                k = getunkey(key);
+            }
+
+            string result = "";
+            for (int i = 0; i < (1 + (msg.Length / k.Length)); i++)
+            {
+
+                if ((i + 1) * k.Length > msg.Length)
+                {
+                    return result += msg.Substring(i * k.Length, msg.Length - (i * k.Length));
+                }
+                tmp1 = msg.Substring(i * k.Length, k.Length);
+                tmp2 = "";
+                for (int n = 0; n < k.Length; n++)
+                {
+                    tmp2 += tmp1.Substring(k[n] - 1, 1);
+                }
+                result += tmp2;
+            }
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (state)
+            {
+            case 1:
+                {
+                    groupBox1.Text = "Криптограмма";
+                    button1.Text = "Расшифровать";
+                    textBox1.Text = crypt(textBox1.Text, true);
+                    state = 2;
+                    break;
+                }
+
+                case 2:
+                {
+                    groupBox1.Text = "Исходный текст";
+                    button1.Text = "Зашифровать";
+                    textBox1.Text = crypt(textBox1.Text, false);
+                    state = 1;
+                    break;
+                }
+                default:
+                {
+                    MessageBox.Show("Error");
+                    break;
+                }
+            }
+        }
+    }
+}
